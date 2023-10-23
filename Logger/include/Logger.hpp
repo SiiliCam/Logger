346337de812481ba39c3316bc6e6d22115ea0645
Simo Rautiainen <simo.rautiainen@gmail.com>
2023-10-23 10:29:39 +0300

    made logger into singleton so it actually works now across submodules

--- Logger/include/Logger.hpp
+++ Logger/include/Logger.hpp
@@ -39,7 +39,9 @@ static std::string getCurrentTimeWithMilliseconds() {
 
     return oss.str();
 }
+
 namespace Logger {
+
     enum class LogLevel {
         info,
         error,
@@ -52,117 +54,138 @@ namespace Logger {
         std::string message;
     };
 
-    static bool initialized = false;
-    static bool is_running = true;
-    static std::mutex log_mutex;
-    static std::mutex queue_mutex;
-    static std::condition_variable condition_var;
-    static std::queue<LogMessage> log_queue;
-    static std::ofstream log_file;
+    class Logger {
+    private:
+        bool initialized = false;
+        bool is_running = true;
+        std::mutex log_mutex;
+        std::mutex queue_mutex;
+        std::condition_variable condition_var;
+        std::queue<LogMessage> log_queue;
+        std::ofstream log_file;
 
-    inline void write_log(const LogMessage& log_msg) {
-        auto now = std::chrono::system_clock::now();
-        auto time_t_now = std::chrono::system_clock::to_time_t(now);
-        std::string timestamp = getCurrentTimeWithMilliseconds();
+        Logger() {}
 
-        std::string level_str;
-        std::string color_code;
+        template<typename... Args>
+        void enqueue_message(LogLevel level, Args... args) {
+            std::ostringstream stream;
+            ((stream << args << " "), ...);
+            LogMessage log_msg{ level, stream.str() };
 
-        switch (log_msg.level) {
-        case LogLevel::info:
-            level_str = "info";
-            color_code = "\033[32m";  // ANSI code for green
-            break;
-        case LogLevel::error:
-            level_str = "error";
-            color_code = "\033[31m";  // ANSI code for red
-            break;
-        case LogLevel::warning:
-            level_str = "warning";
-            color_code = "\033[33m";  // ANSI code for yellow
-            break;
-        default:
-            level_str = "trace";
-            color_code = "\033[0m";  // ANSI code for default
-            break;
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            log_queue.push(log_msg);
+            condition_var.notify_one();
         }
 
-        std::string log_entry = "[" + timestamp + "][" + level_str + "]\t" + log_msg.message + "\n";
-
-        // Bold for timestamp is \033[1m and \033[0m to reset
-        std::string colored_log_entry = "\033[1m[" + timestamp + "]\033[0m[" + color_code + level_str + "\033[0m]\t" + log_msg.message + "\n";
-
-        std::cout << colored_log_entry;  // Colored and bolded console log
-
-        if (log_file.is_open()) {
-            log_file << log_entry;  // File log (uncolored)
+    public:
+        static Logger& getInstance() {
+            static Logger instance;
+            return instance;
         }
-    }
 
+        void init_logging(const std::string& file) {
+            log_file.open(file, std::ios::out | std::ios::app);
+            initialized = true;
+            std::thread log_thread(&Logger::logging_thread, this);
+            log_thread.detach();
+        }
+        void write_log(const LogMessage& log_msg) {
+            auto now = std::chrono::system_clock::now();
+            auto time_t_now = std::chrono::system_clock::to_time_t(now);
+            std::string timestamp = getCurrentTimeWithMilliseconds();
 
-    inline void logging_thread() {
-        while (is_running) {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            condition_var.wait(lock, [] { return !log_queue.empty() || !is_running; });
-            while (!log_queue.empty()) {
-                LogMessage log_msg = log_queue.front();
-                log_queue.pop();
-                write_log(log_msg);
+            std::string level_str;
+            std::string color_code;
+
+            switch (log_msg.level) {
+            case LogLevel::info:
+                level_str = "info";
+                color_code = "\033[32m";  // ANSI code for green
+                break;
+            case LogLevel::error:
+                level_str = "error";
+                color_code = "\033[31m";  // ANSI code for red
+                break;
+            case LogLevel::warning:
+                level_str = "warning";
+                color_code = "\033[33m";  // ANSI code for yellow
+                break;
+            default:
+                level_str = "trace";
+                color_code = "\033[0m";  // ANSI code for default
+                break;
             }
-            lock.unlock();
+
+            std::string log_entry = "[" + timestamp + "][" + level_str + "]\t" + log_msg.message + "\n";
+
+            // Bold for timestamp is \033[1m and \033[0m to reset
+            std::string colored_log_entry = "\033[1m[" + timestamp + "]\033[0m[" + color_code + level_str + "\033[0m]\t" + log_msg.message + "\n";
+
+            std::cout << colored_log_entry;  // Colored and bolded console log
+
+            if (log_file.is_open()) {
+                log_file << log_entry;  // File log (uncolored)
+                log_file.flush();
+            }
+            else {
+                std::cout << "log file is not open";
+            }
         }
-    }
+        void logging_thread() {
+            while (is_running) {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                condition_var.wait(lock, [this] { return !log_queue.empty() || !is_running; });
+                while (!log_queue.empty()) {
+                    LogMessage log_msg = log_queue.front();
+                    log_queue.pop();
+                    write_log(log_msg);
+                }
+                lock.unlock();
+            }
+        }
 
-    inline void init_logging(const std::string& file) {
-        log_file.open(file, std::ios::out | std::ios::app);
-        initialized = true;
-        std::thread log_thread(logging_thread);
-        log_thread.detach();
-    }
+        template<typename... Args>
+        void log_info(Args... args) {
+            if (!initialized) {
+                init_logging("default.log");
+            }
+            enqueue_message(LogLevel::info, args...);
+        }
 
-    inline void init_console_logging() {
-        initialized = true;
-        std::thread log_thread(logging_thread);
-        log_thread.detach();
-    }
-
-    inline void shutdown() {
-        is_running = false;
-        condition_var.notify_one();
-    }
+        template<typename... Args>
+        void log_error(Args... args) {
+            if (!initialized) {
+                init_logging("default.log");
+            }
+            enqueue_message(LogLevel::error, args...);
+        }
+        template<typename... Args>
+        inline void log_warn(Args... args) {
+            if (!initialized) {
+                init_console_logging("default.log");
+            }
+            enqueue_message(LogLevel::warning, args...);
+        }
+        // Other log levels here...
+    };
 
     template<typename... Args>
-    inline void enqueue_message(LogLevel level, Args... args) {
-        std::ostringstream stream;
-        ((stream << args << " "), ...);
-        LogMessage log_msg{ level, stream.str() };
-
-        std::lock_guard<std::mutex> lock(queue_mutex);
-        log_queue.push(log_msg);
-        condition_var.notify_one();
+    inline void init_logging(const std::string& file, Args... args) {
+        Logger::getInstance().init_logging(file);
     }
 
     template<typename... Args>
     inline void log_info(Args... args) {
-        if (!initialized) {
-            init_console_logging();
-        }
-        enqueue_message(LogLevel::info, args...);
+        Logger::getInstance().log_info(args...);
     }
 
     template<typename... Args>
     inline void log_error(Args... args) {
-        if (!initialized) {
-            init_console_logging();
-        }
-        enqueue_message(LogLevel::error, args...);
+        Logger::getInstance().log_error(args...);
     }
-
     template<typename... Args>
     inline void log_warn(Args... args) {
-        if (!initialized) {
-            init_console_logging();
-        }
-        enqueue_message(LogLevel::warning, args...);
+        Logger::getInstance().log_warn(args...);
     }
+    // Add other logging methods as needed
 }
